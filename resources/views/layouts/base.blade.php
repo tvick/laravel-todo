e17d3c2f4371bcf6974950442850042cecbb52dd
Tim Vick <t.vickc@gmail.com>
2017-02-08 13:48:35 -0800

    added todolists/index.blade.php, and setup route

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/g/normalize@5.0.0,foundation@6.2.4(foundation.min.css)">
+        <script src="https://cdn.jsdelivr.net/g/modernizr@3.3.1,foundation@6.2.4"></script>
+    </head>
+    <body>
+        <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+        @yield('body')
+    </body>
+</html>
---
+++ resources/views/todolists/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.base')
+
+@section('body')
+<div class="row column">
+
+    <h1>Todo Lists</h1>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Name 1</td>
+            </tr>
+            <tr>
+                <td>Name 2</td>
+            </tr>
+        </tbody>
+    </table>
+
+</div>
+@endsection
