ded6a01889c7ad7cbb170392c51493bc680d0c43
Tim Vick <t.vickc@gmail.com>
2017-02-09 17:08:00 -0800

    added to create template

--- resources/views/todolists/create.blade.php
+++ resources/views/todolists/create.blade.php
@@ -6,7 +6,9 @@
     <h1>Add New Todo List</h1>
 
     <form method="POST" action="{{ route('todolists.store') }}">
+
         {{ csrf_field() }}
+
         <div class="row">
             <div class="columns">
                 <label>Input Label
@@ -14,6 +16,7 @@
                 </label>
             </div>
         </div>
+        
         <div class="row">
             <div class="columns">
                 <input type="submit" class="button" value="Submit" />
